49d36ce4b4c6facfe77cfbe7e2e8ecc0de77bf06
nicholas russell <nrussell97@internode.on.net>
2013-06-30 12:10:49 +1000

    Combined the notices and page scraper into one class, Scraper

--- src/api.php
+++ src/api.php
@@ -1,25 +1,19 @@
 <?php 
 include('simple_html_dom.php');
 
-class Page {
-	
+class Scraper {
 	function getPage($pageId) {
 		$externalHtml = file_get_html('http://www.smccsydney.catholic.edu.au/page/display/id/' . $pageId);
 		foreach($externalHtml->find('div#maincol') as $e)
     		echo $e->innertext;
 	}
-
-
-}
-
-class Notices {
-
 	function getNotices() {
 		$externalHtml = file_get_html('http://www.smccsydney.catholic.edu.au/');
 		foreach ($externalHtml->find('div#message') as $e) {
 			echo $e->innertext;
 		}
 	}
+
 }
 
 ?>
