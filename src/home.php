955084dc68e5ce694dd0bb04156c1f7981806ef1
nicholas russell <nrussell97@internode.on.net>
2013-08-28 09:44:37 +1000

    Added bootstrap 3.0

---
+++ src/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>SMCC Sydney</title>
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/css/globalstylesheet.css" rel="stylesheet">
+</head>
+<body>
+	
+	
+	<!--Javascript at the bottom to reduce loading time-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	
+</body>
+</html>
