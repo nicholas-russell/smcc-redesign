570d6023679cb2faf4b6242fed46bac02ee6ce72
nicholas russell <nrussell97@internode.on.net>
2013-09-02 14:11:44 +1000

    Changed the template to include these

---
+++ src/template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>SMCC</title>
+<?php
+	
+	require('assets/html/head.html');
+	
+?>
+</head>
+<body>
+<?php
+	
+	require('assets/html/navbar.html');
+	
+?>
+	<div id="body">
+		<div class="container" id="bodycontainer">
+			
+			<div class="row">
+				<div class="col-md-12">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquam lectus ac imperdiet posuere. Sed sed adipiscing ante. Aenean fringilla elit sed convallis imperdiet. Donec molestie imperdiet ante, ac scelerisque nisi feugiat in. Etiam id lacus ut lectus varius fermentum.</p>
+				</div>
+			</div>
+		</div>
+	<?php
+		require('assets/html/footer.html');
+	?>
+	</div>
+	</body>
+</html>
